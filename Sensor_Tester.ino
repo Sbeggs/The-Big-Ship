6e28a3f3e22588a262778cc9aa0b5e79dcc1ae57
Sbeggs <44120953+Sbeggs@users.noreply.github.com>
2018-11-06 00:56:33 -0500

    Add files via upload

---
+++ Sensor_Tester.ino
@@ -0,0 +1,73 @@
+
+const int ledPin = 13; // the pin that the LED is attached to
+int incomingByte;      // a variable to read incoming serial data into
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  Serial.println("Hey There I am ready");
+}
+
+void loop() {
+  // see if there's incoming serial data:
+ 
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+    // if it's a w flash once slowly
+
+
+    if (incomingByte == 'w') {
+      digitalWrite(ledPin, HIGH);
+      delay(3000);
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+      Serial.println("z");
+    }
+    // if it's a d flash twice normal
+    if (incomingByte == 'd') {
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      Serial.println("z"); // tell em we're done
+    }
+    if (incomingByte == 'a') { // if left flash thrice slowly
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      digitalWrite(ledPin, HIGH);
+      delay(50 00);
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      Serial.println("z");
+    }
+
+    if (incomingByte == 's') { // if left flash thrice slowly
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+      digitalWrite(ledPin, HIGH);
+      delay(500);
+      digitalWrite(ledPin, LOW);
+      delay(500);
+      Serial.println("z");
+    }
+  }
+}
