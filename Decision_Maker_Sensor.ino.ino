32540703d19420f4abdd2055d2333a07047e6272
Sbeggs <44120953+Sbeggs@users.noreply.github.com>
2018-11-07 00:50:43 -0500

    Add files via upload

---
+++ Decision_Maker_Sensor.ino.ino
@@ -0,0 +1,410 @@
+// define all Digital Pins for Mega(Even is Trig, Odd is Echo)
+#define TrigB 34
+#define EchoB 35
+#define TrigFR 26
+#define EchoFR 27
+#define TrigFL 24
+#define EchoFL 25
+#define TrigF 32
+#define EchoF 33
+#define TrigR 28
+#define EchoR 29
+#define TrigL 30
+#define EchoL 31
+
+// define various Parameters
+#define WaitForEcho 10 // how long the US sensors wait for the echo (in microseconds)
+#define WackValue 100000 // the Value above which we consider the value not real
+#define DurationCheck 40 // Check to see if old duration is different enough from current reading
+#define MinObsDis 15 // distance we consider to be unsafe in cm for basic cardinal directions
+#define MinDistance 9// What we consider too close to wall for allignment
+#define sensorSeperation 5 // space between sensors
+#define TurningTime 2500 // in millisex
+#define RealignValue 1.1 // the difference between two values that triggers a re-allign
+
+//Declaring the Distance Array
+double DisArray[6];
+// DisArray is indexed as follows *0* is Forward, *1* is Forward Right, *2* is Forward Left, *3* Rear R, *4* Rear L, *5* is backwards
+char Names [8] = {'F', 'F', 'R', 'F', 'L', 'R', 'L', 'B'}; // names of Values for read outs, just for debugging
+unsigned int TimeOut = 0;
+
+long durationF;
+double distanceF;
+long old_durationF = 0;
+long durationFR;
+double distanceFR;
+long old_durationFR = 0;
+long durationFL;
+double distanceFL;
+long old_durationFL = 0;
+long durationR;
+double distanceR;
+long old_durationR = 0;
+long durationL;
+double distanceL;
+long old_durationL = 0;
+long durationB;
+double distanceB;
+long old_durationB = 0;
+int KylesClear;
+
+double theta = 0;
+
+void setup() {
+  pinMode(TrigF, OUTPUT);
+  pinMode(EchoF, INPUT);
+  pinMode(TrigFR, OUTPUT);
+  pinMode(EchoFR, INPUT);
+  pinMode(TrigFL, OUTPUT);
+  pinMode(EchoFL, INPUT);
+  pinMode(TrigR, OUTPUT);
+  pinMode(EchoR, INPUT);
+  pinMode(TrigL, OUTPUT);
+  pinMode(EchoL, INPUT);
+  pinMode(TrigB, OUTPUT);
+  pinMode(EchoB, INPUT);
+  Serial.begin(9600);
+
+
+  delay(3000);
+
+}
+
+void loop() {
+  /* The loop is where we will read all the sensor data, this is the default state of everything, once it receives all the values
+      it checks forward. T
+  */
+
+  //turns off trigger
+  digitalWrite(TrigF, LOW);
+  digitalWrite(TrigB, LOW);
+  digitalWrite(TrigFR, LOW);
+  digitalWrite(TrigFL, LOW);
+  digitalWrite(TrigR, LOW);
+  digitalWrite(TrigL, LOW);
+  delayMicroseconds(2);
+
+
+  // Sets TriggerF on for WaitForEcho useconds
+  digitalWrite(TrigF, HIGH);
+  delayMicroseconds(WaitForEcho);
+  digitalWrite(TrigF, LOW);
+
+  // reads the time it took for the EchoF to reach back
+  durationF = pulseIn(EchoF, HIGH);
+
+
+
+  // Sets TriggerB on for WaitForEcho useconds, can't do two at same time for w.e reason
+  digitalWrite(TrigB, HIGH);
+  delayMicroseconds(WaitForEcho);
+  digitalWrite(TrigB, LOW);
+  // reads the time it took for the EchoB to reach back
+  durationB = pulseIn(EchoB, HIGH);
+
+  // Sets TriggerFR on for WaitForEcho useconds, can't do two at same time for w.e reason
+  digitalWrite(TrigFR, HIGH);
+  delayMicroseconds(WaitForEcho);
+  digitalWrite(TrigFR, LOW);
+  // reads the time it took for the EchoFR to reach back
+  durationFR = pulseIn(EchoFR, HIGH);
+
+  // Sets TriggerFL on for WaitForEcho useconds, can't do two at same time for w.e reason
+  digitalWrite(TrigFL, HIGH);
+  delayMicroseconds(WaitForEcho);
+  digitalWrite(TrigFL, LOW);
+  // reads the time it took for the EchoFL to reach back
+  durationFL = pulseIn(EchoFL, HIGH);
+
+
+  // Sets TriggerR on for WaitForEcho useconds, can't do two at same time for w.e reason
+  digitalWrite(TrigR, HIGH);
+  delayMicroseconds(WaitForEcho);
+  digitalWrite(TrigR, LOW);
+  // reads the time it took for the EchoB to reach back
+  durationR = pulseIn(EchoR, HIGH);
+
+  // Sets TriggerL on for WaitForEcho useconds, can't do two at same time for w.e reason
+  digitalWrite(TrigL, HIGH);
+  delayMicroseconds(WaitForEcho);
+  digitalWrite(TrigL, LOW);
+  // reads the time it took for the EchoL to reach back
+  durationL = pulseIn(EchoL, HIGH);
+
+
+  // Filters out wack values, may not need to exist
+  if (durationF > WackValue) durationF = old_durationF;
+  if (durationB > WackValue) durationB = old_durationB;
+  if (durationFR > WackValue) durationFR = old_durationFR;
+  if (durationFL > WackValue) durationFL = old_durationFL;
+  if (durationR > WackValue) durationR = old_durationR;
+  if (durationL > WackValue) durationL = old_durationL;
+
+  // Checks to see if change in duration, if not calculate distance and print!
+  if (abs(old_durationF - durationF) > DurationCheck) {
+    distanceF = .0343 * durationF / 2;
+
+  }
+  if (abs(old_durationB - durationB) > DurationCheck) {
+    distanceB = .0343 * durationB / 2;
+
+  }
+
+  if (abs(old_durationFR - durationFR) > DurationCheck) {
+    distanceFR = .0343 * durationFR / 2;
+
+  }
+  if (abs(old_durationFL - durationFL) > DurationCheck) {
+    distanceFL = .0343 * durationFL / 2;
+
+  }
+  if (abs(old_durationR - durationR) > DurationCheck) {
+    distanceR = .0343 * durationR / 2;
+
+  }
+  if (abs(old_durationL - durationL) > DurationCheck) {
+    distanceL = .0343 * durationL / 2;
+
+  }
+  DisArray[0] = distanceF;
+  DisArray[1] = distanceFR;
+  DisArray[2] = distanceFL;
+  DisArray[3] = distanceR;
+  DisArray[4] = distanceL;
+  DisArray[5] = distanceB;
+  //delay(100);
+
+  ////  never delete this!!!!!!!!! Debugging uses Prints out array of distances for easy debugging
+  Serial.print(Names[0]);
+  Serial.print(",    ");
+  Serial.print(Names[1]);
+  Serial.print(Names[2]);
+  Serial.print(",    ");
+  Serial.print(Names[3]);
+  Serial.print(Names[4]);
+  Serial.print(",    ");
+  Serial.print(Names[5]);
+  Serial.print(",    ");
+  Serial.print(Names[6]);
+  Serial.print(",    ");
+  Serial.print(Names[7]);
+  Serial.println("");
+  for (int i = 0; i <= 5; i++) {
+    Serial.print(DisArray[i]);
+    Serial.print(", ");
+  }
+  Serial.println("");
+  Serial.println("");
+
+
+  checkForward(DisArray); // checks forward path
+
+  old_durationF = durationF;
+  old_durationB = durationB;
+  old_durationFR = durationFR;
+  old_durationFL = durationFL;
+  old_durationR = durationR;
+  old_durationL = durationL;
+
+
+}
+
+//
+void checkForward(double A[6]) {
+  /* function that checks the forward IR sensor (first value in array) is sensing anything in front of it,
+      if not move forward function, wait for forward function to complete and then return to read values
+      if so it begins side check
+  */
+  //Serial.println("Checking Forward!");
+  if (A[0] >= MinObsDis) {
+    checkAlignment(); 
+    // check allignment before movement to make sure we are alligned, we want to go all the way too loop after this doe, I can't figure 
+    //out how to do it
+    moveForward();
+    checkAlignment();
+    return;
+  }
+  else {
+    // stopLocomotion(); possible function that sends stop signal
+    checkSides(A);
+    return;
+  }
+
+  //checkAlignment(); //Check alignment and turn if need be
+
+}
+
+void checkSides(double A[6]) {
+  /* Checks to see if Right side has anything within distance, if so turn right wait for that to complete and then check allignment
+     if not check left, if left is good turn left (make three right turns) and return to read some values
+     if not turn around and go backwards and return to read some distances
+  */
+
+  //
+  if (A [1] >= MinObsDis) {
+    turnRight();
+    checkAlignment();
+    return;
+
+  }
+  else if (A [2] >= MinObsDis) {
+    //Serial.println("Turning Left!");
+    turnLeft();
+    checkAlignment();
+    return;
+  }
+  else if ( A [5] >= MinObsDis) {
+    // Serial.println("Going Back!");
+
+    moveBack();
+    checkAlignment();
+
+    return;
+  }
+
+  //  else realignRight();
+
+}
+
+void moveForward() {
+  Serial.println("w"); // sends move Forward to the Uno
+  waitForResponse();
+}
+
+
+void turnRight() {
+
+  Serial.println("d"); // sends turn right function to other guy
+  waitForResponse();
+}
+
+void turnLeft() {
+  Serial.println("a"); // sends turn left function to other guy
+  waitForResponse();
+}
+
+
+void moveBack() {
+  Serial.println("s"); // sends move back command to other guy
+  waitForResponse();
+}
+
+void realignRight() {
+  Serial.println("e"); // sends move back command to other guy
+  waitForResponseRealign();
+}
+
+void realignLeft() {
+  Serial.println("q"); // sends move back command to other guy
+  waitForResponseRealign();
+}
+
+void checkAlignment () {
+  /* Called before every forward movement, but turns are favoured over check allignment
+      This function checks to see if we are within range for a reallignment, if we are checks to see if we are going to be movin
+      towards or away from the slanted wall, if towards it calculates a theta value and tells us to reallign and returns to read
+      more values
+  */
+  double FR = DisArray[1];
+  double FL = DisArray[2];
+  double R = DisArray[3];
+  double L = DisArray[4];
+  //  bool cantTurnRight = FALSE;
+  //  bool cantTurnLeft = FALSE;
+
+  //  if (R < noTurnDistance) cantTurnLeft = TRUE;
+  //  if (L < noTurnDistance) cantTurnRight = TRUE;
+
+
+
+  if (FL < MinDistance || L < MinDistance) {
+    Serial.println("Left Allign");
+    //    Serial.println(L - FL);
+    theta = atan((FL - L) / sensorSeperation) * 360 / 6.28; // checks to see where we are compared to wall
+    Serial.println(theta);
+    if ((L - FL) > RealignValue) {
+
+      Serial.println("In theta if");
+      while (theta < 0) {// probably doesn't really need to exist anymore, just becomes part of our loop
+        //        Serial.println(theta);
+        realignRight();
+        delay(200);
+        return; // brings us back and we move forwards agian
+      }
+    }
+  }
+
+  else if (FR < MinDistance || R < MinDistance) {
+    Serial.println("Right Allign");
+
+    theta = atan((FR - R) / sensorSeperation) * 360 / 6.28; // checks to see where we are compared to wall
+    Serial.println(theta);
+    if ((R - FR) > RealignValue) {
+      Serial.println("In theta if");
+
+      while (theta < 0) { // probably doesn't really need to exist anymore, just becomes part of our loop
+        Serial.println(theta);
+        realignLeft();
+        return;
+        //        theta = theta + 20;
+      }
+    }
+  }
+}
+
+//void checkAlign () {
+//  /* Function that checks allignment, looks to see if the two R sensors are within a range (small enough = close to wall),
+//      checks how different they are and sends value to reallign, if they are out of range we don't care
+//     Does the same for the left
+//  */
+//  double Difference;
+//  double Angle = 0;
+//  if (DisArray[1] < Close && DisArray[3] < Close ) {
+//    Difference = (DisArray[1] - DisArray [3]);
+//    if (abs(Difference) > 1.5) {
+//      //if (Difference > 0) SpinRight = true;
+//      //else SpinRight = false;
+//      Angle = asin (Difference / SensorSpace);
+//    }
+//
+//
+//
+//  }
+//  else if (DisArray[2] < Close && DisArray[4] < Close) {
+//    Difference = DisArray[2] - DisArray [4];
+//    if (abs(Difference) > 1.5) {
+//      //if (Difference > 0) SpinRight = false;
+//      // else SpinRight = true;
+//      Angle = asin(Difference / SensorSpace);
+//    }
+//
+//  }
+//
+//  return;
+//}
+
+void waitForResponse() {
+  // Function that waits for a response
+  delay(200);
+  //  KylesClear = Serial.read();
+  while (Serial.available() == 0) {
+    if (++TimeOut == TurningTime)break; // waits for turning time, if matt+katherine's code blows up, this resets us
+    delay(1);
+  }
+  KylesClear = Serial.read();
+  TimeOut = 0;
+  delay(500);
+}
+
+void waitForResponseRealign() {
+  // Function that waits for a response
+  delay(200);
+  //  KylesClear = Serial.read();
+  while (Serial.available() == 0) {
+    if (++TimeOut == TurningTime)break; // waits for turning time, if matt+katherine's code blows up, this resets us
+    delay(1);
+  }
+  KylesClear = Serial.read();
+  TimeOut = 0;
+  delay(500);
+}
